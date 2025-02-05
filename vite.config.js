5389a0fcf458459b07424f5ded1a8c8fe50241d0
CrisUriarte <cristobal.uriarte@hotmail.com>
2025-02-05 19:38:15 -0300

    vite host

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,9 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss(),
   ],
+  server: {
+    allowedHosts: ["flapp-front-serv.onrender.com"],  // 🔹 Permitir Render
+    host: true,  // 🔹 Permitir acceso externo
+    port: 5173,  // Asegurar que el puerto es 3000
+  }
 })
