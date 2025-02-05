3c2936f482bfe0dafb4468cd49489b4114b12425
CrisUriarte <cristobal.uriarte@hotmail.com>
2025-02-05 18:50:57 -0300

    endpointback

--- src/services/productServices.jsx
+++ src/services/productServices.jsx
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api/cart/";
+const API_URL = import.meta.env.VITE_API_URL || "https://flapp-back.onrender.com/api/cart/";
 
 export const fetchProducts = async () => {
   try {
